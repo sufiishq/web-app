2895ca5bf2f712facefc00225279b08e5a75f922
Farhan <developer.farhanfahim@gmail.com>
2023-04-05 22:06:14 +0500

    dark/light theme change button added

--- lib/core/utils/initial_bindings.dart
+++ lib/core/utils/initial_bindings.dart
@@ -1,8 +1,12 @@
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sufi_ishq/core/app_export.dart';
 
 class InitialBindings extends Bindings {
   @override
   void dependencies() {
-    Get.put(PrefUtils());
+    Get.putAsync<SharedPreferences>(() async {
+      final prefs = await SharedPreferences.getInstance();
+      return prefs;
+    }, permanent: true);
   }
 }
--- lib/presentation/home_screen/controller/home_controller.dart
+++ lib/presentation/home_screen/controller/home_controller.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sufi_ishq/core/utils/constant.dart';
 import 'package:sufi_ishq/core/utils/local_db_key.dart';
+
+import '../../../core/utils/pref_utils.dart';
 
 class HomeController extends GetxController
     with GetSingleTickerProviderStateMixin {
   RxBool isLightTheme = false.obs;
-  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
   AnimationController? animationController;
   Animation<Offset>? animation;
 
@@ -29,15 +29,12 @@ class HomeController extends GetxController
   }
 
   saveThemeStatus() async {
-    SharedPreferences pref = await _prefs;
-    pref.setBool(LocalDBKeys.theme, isLightTheme.value);
+    PrefUtils.storage.setBool(LocalDBKeys.theme, isLightTheme.value);
   }
 
   getThemeStatus() async {
-    var isLight = _prefs.then((SharedPreferences prefs) {
-      return prefs.getBool(LocalDBKeys.theme) ?? true;
-    }).obs;
-    isLightTheme.value = await isLight.value;
+    dynamic isLight = PrefUtils.storage.getBool(LocalDBKeys.theme) ?? true;
+    isLightTheme.value = await isLight;
     Get.changeThemeMode(isLightTheme.value ? ThemeMode.light : ThemeMode.dark);
     isLightTheme.value
         ? animationController!.forward()
